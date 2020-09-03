6f14d82b3105abca6abf7eabe70e4d2a27195220
MrEverything97 <nampp1204@gmail.com>
2020-09-03 13:50:45 +0700

    push them

--- src/app/service/searchService/employee-search-service.service.ts
+++ src/app/service/searchService/employee-search-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Employee} from '../../interface/employee';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeSearchServiceService {
 
+  employee: Employee[] = [];
+  value = new BehaviorSubject('');
+  list = new BehaviorSubject(this.employee);
   constructor() { }
+  changeValue(message: string, employee: Employee[]) {
+    this.value.next(message);
+    this.list.next(employee);
+  }
 }
