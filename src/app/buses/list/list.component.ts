d0d0866aa90a81dc4d4931d60d695c36a804b089
MrEverything97 <nampp1204@gmail.com>
2020-09-03 13:49:16 +0700

    search buses by location

--- src/app/buses/list/list.component.ts
+++ src/app/buses/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Buses} from '../../interface/buses';
 import {BusesService} from '../../service/buses.service';
-
+import {BusesSearchServiceService} from '../../service/searchService/buses-search-service.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -11,12 +11,15 @@ export class ListComponent implements OnInit {
 
   busesList: Buses[] = [];
   failMessage: string;
-  constructor(private busesService: BusesService) { }
+  keyword: any;
+  constructor(private busesService: BusesService,
+              private busesSearchService: BusesSearchServiceService) { }
 
   ngOnInit(): void {
     this.busesService.showBusesList()
       .subscribe(result => {
         this.busesList = result;
+        console.log(this.keyword as number);
       }, error => {
         this.failMessage = 'SHOW BUSES LIST  FAIL !';
       });
@@ -27,5 +30,18 @@ export class ListComponent implements OnInit {
     }, error => {
       console.log('delete failed');
     });
+  }
+  search(){
+    if (this.keyword !== ''){
+        this.busesService.findByLocation(this.keyword).subscribe( data => {
+          this.busesList = data;
+          this.busesSearchService.changeValue(this.keyword, this.busesList);
+        });
+    }else {
+      this.busesService.showBusesList().subscribe( data => {
+        this.busesList = data;
+        this.busesSearchService.changeValue(this.keyword, this.busesList);
+      });
+    }
   }
 }
--- src/app/service/buses.service.ts
+++ src/app/service/buses.service.ts
@@ -8,6 +8,8 @@ import {Buses} from '../interface/buses';
 })
 export class BusesService {
   private readonly API_BUSES = 'http://localhost:8080/buses'; // khai bao API trung voi request mapping trong backend
+  private readonly API_FIND_BUSES_BY_LOCATION = 'http://localhost:8080/buses/find-by-location';
+  // private readonly API_FIND_BUSES_BY_DISTANCE = 'http://localhost:8080/buses/find-by-distance';
   constructor(private httpClient: HttpClient) { }
 
   showBusesList(): Observable<Buses[]>{
@@ -25,5 +27,11 @@ export class BusesService {
   deleteBusesByID(id: number): Observable<Buses> {
     return this.httpClient.delete<Buses>(`${this.API_BUSES}/delete/` + `${id}`);
   }
+  findByLocation(location: string): Observable<Buses[]>{
+    return this.httpClient.get<Buses[]>(`${this.API_FIND_BUSES_BY_LOCATION}/${location}`);
+  }
+  // findByDistance(distance: number): Observable<Buses[]>{
+  //   return this.httpClient.get<Buses[]>(`${this.API_FIND_BUSES_BY_DISTANCE}/${distance}`);
+  // }
 
 }
